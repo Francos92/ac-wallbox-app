97b0f9dd5707782518675c02fdf568c9b3bc5819
Francos92 <francescocoralluzzo2@gmail.com>
2026-08-23 11:41:49 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // (JS/CSS-Bundles, PDF-Vorlage, Icons) wird beim ersten Laden automatisch
 // zwischengespeichert und danach auch ohne Internetverbindung geladen.
 
-const CACHE_NAME = 'braba-prueftool-1787476195296';
+const CACHE_NAME = 'braba-prueftool-1787478101972';
 const APP_SHELL = [
   '/',
   '/index.html',
